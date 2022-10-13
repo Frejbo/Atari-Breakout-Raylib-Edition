6b9f2221dc3b4a2667c638b2dac27bc2f7bafbdc
Frejbo <frej.hellsten@gmail.com>
2022-10-13 18:10:26 +0200

    Block

--- Top_down/Program.cs
+++ Top_down/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 
-int speed = 5;
+int speed = 10;
 
 
 Raylib.InitWindow(1024, 1024, "TopDownGame");
@@ -12,12 +12,20 @@ Rectangle platta = new Rectangle(412, 900, 200, 30);
 
 Vector2 ball = new Vector2(512, 512);
 
+List<Rectangle> blocks = new List<Rectangle>();
+Vector2 block_size = new Vector2(40, 20);
+int blocks_gap = 10;
 
-// for (int i = 0; i < 10; i++) {
-// }
+for (int x = 1; x <= 20; x++) {
+    for (int y = 1; y <= 5; y++) {
+        blocks.Add(new Rectangle((block_size.X + blocks_gap) * x, (block_size.Y + blocks_gap) * y, block_size.X, block_size.Y));
+    }
+}
 
 Vector2 ball_velocity = new Vector2(1, 1);
-const int ball_speed = 2;
+const int ball_speed = 5;
+
+Vector2 prev_ball_pos;
 
 while (!Raylib.WindowShouldClose()) {
     if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) {
@@ -29,7 +37,9 @@ while (!Raylib.WindowShouldClose()) {
 
 
     // ball
-    if(ball.X < 1 || ball.X > (1024 - bollBild.width)) {
+    if ((ball.X < 1 || ball.X > (1024 - bollBild.width)) && (ball.Y < 1 || ball.Y > (1024 - bollBild.height))) {
+        ball_velocity = -ball_velocity;
+    } else if(ball.X < 1 || ball.X > (1024 - bollBild.width)) {
         ball_velocity.X -= (ball_velocity.X*2);
     } else if (ball.Y < 1 || ball.Y > (1024 - bollBild.height)) {
         ball_velocity.Y -= (ball_velocity.Y*2);
@@ -38,16 +48,33 @@ while (!Raylib.WindowShouldClose()) {
         ball_velocity.Y -= (ball_velocity.Y*2);
     }
 
+    List<Rectangle> remove_blocks = new List<Rectangle>();
+    foreach (Rectangle block in blocks) {
+        bool colliding = Raylib.CheckCollisionCircleRec(ball, bollBild.width, block);
+        if (colliding) {
+            remove_blocks.Add(block);
+            // bounce
+            
+        }
+    }
+    foreach (Rectangle block in remove_blocks) {blocks.Remove(block);}
+
 
 
     ball.X += ball_velocity.X * ball_speed;
     ball.Y += ball_velocity.Y * ball_speed;
+
+    prev_ball_pos = ball;
 
 
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.BLACK);
 
     Color white = new Color(255, 255, 255, 255);
+    
+    foreach (Rectangle block in blocks) {
+        Raylib.DrawRectangleRec(block, white);
+    }
 
     Raylib.DrawRectangleRec(platta, white);
     // Raylib.DrawCircleV(ball, 20, white);
