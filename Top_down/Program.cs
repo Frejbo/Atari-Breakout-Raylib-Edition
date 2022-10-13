2b076f18551de4640699ff4b9131159f5de7a082
Frejbo <frej.hellsten@gmail.com>
2022-10-13 15:28:37 +0200

    Base

--- Top_down/Program.cs
+++ Top_down/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+Raylib.InitWindow(1024, 1024, "TopDownGame");
+Raylib.SetTargetFPS(60);
+
+Rectangle character = new Rectangle(0, 0, 30, 30);
+
+while (!Raylib.WindowShouldClose()) {
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) {
+        character.x = 512;
+        character.y = 512;
+    }
+
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.BLACK);
+    Raylib.EndDrawing();
+
+    Raylib.DrawRectangleRec(character, new Color(255, 255, 255, 255));
+}
