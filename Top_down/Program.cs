25ca46bc808f73345cad67376ab2540e8dd25414
Frejbo <frej.hellsten@gmail.com>
2022-10-14 12:58:24 +0200

    Kan förlora och vinna

--- Top_down/Program.cs
+++ Top_down/Program.cs
@@ -5,6 +5,7 @@ int speed = 10;
 int health = 3;
 
 Vector2 screen_size = new Vector2(1280, 720);
+Rectangle screen_rect = new Rectangle(0, 0, screen_size.X, screen_size.Y);
 Raylib.InitWindow((int)screen_size.X, (int)screen_size.Y, "Atari Breakout");
 Raylib.SetTargetFPS(60);
 
@@ -12,6 +13,8 @@ Texture2D bollBild = Raylib.LoadTexture("Assets/Ball.png");
 Texture2D short_platta = Raylib.LoadTexture("Assets/Short Plate.png");
 Texture2D medium_platta = Raylib.LoadTexture("Assets/Medium Plate.png");
 Texture2D long_platta = Raylib.LoadTexture("Assets/Long Plate.png");
+Texture2D game_over_screen = Raylib.LoadTexture("Assets/Backgrounds/Game over.png");
+Texture2D success_screen = Raylib.LoadTexture("Assets/Backgrounds/Success.png");
 
 List<Texture2D> life = new List<Texture2D>();
 life.Add(Raylib.LoadTexture("Assets/Lifebar/life2.png"));
@@ -23,12 +26,13 @@ Rectangle platta = new Rectangle((screen_size.X/2)-platta_size.X/2, screen_size.
 
 Vector2 ball = new Vector2(screen_size.X/2, screen_size.Y/2);
 
+
 List<Rectangle> blocks = new List<Rectangle>();
 Vector2 block_size = new Vector2(40, 20);
 int blocks_gap = 10;
 
-for (int x = 1; x <= 19; x++) {
-    for (int y = 1; y <= 5; y++) {
+for (int x = 19; x <= 19; x++) {
+    for (int y = 5; y <= 5; y++) {
         blocks.Add(new Rectangle((block_size.X + blocks_gap) * x, (block_size.Y + blocks_gap) * y, block_size.X, block_size.Y));
     }
 }
@@ -37,7 +41,7 @@ Vector2 ball_velocity = new Vector2(0, 1);
 const int ball_speed = 7;
 
 while (!Raylib.WindowShouldClose()) {
-    if (health > 0) {
+    if (life.Count > 0 && blocks.Count > 0) {
         bool bounced_x = false;
         bool bounced_y = false;
 
@@ -66,9 +70,11 @@ while (!Raylib.WindowShouldClose()) {
         } else if (ball.Y < 1) {
             ball_velocity.Y -= (ball_velocity.Y*2);
         } else if (ball.Y > (screen_size.Y - bollBild.height/2)) { // hits bottom
-            ball_velocity.Y -= (ball_velocity.Y*2);
-            health--;
             life.RemoveAt(0);
+            if (life.Count == 0) {continue;}
+            ball.X = screen_size.X/2;
+            ball.Y = screen_size.Y/2;
+            ball_velocity = new Vector2(0, 1);
         } else if (Raylib.CheckCollisionCircleRec(ball, bollBild.width/2, platta)) { // träffar platta
             ball_velocity.Y -= (ball_velocity.Y*2);
             ball_velocity.X = (ball.X - platta.x - (platta_size.X/2))/platta_size.X; // skickar bollens x velocity beroende på var man träffar plattan.
@@ -109,19 +115,30 @@ while (!Raylib.WindowShouldClose()) {
 
         draw_game();
     }
-    else {
+    else if (life.Count == 0) // Death
+    {
 
-    // health <= 0
-    // Death
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.DARKGRAY);
 
-    // Raylib.BeginDrawing();
+    Raylib.DrawTexturePro(game_over_screen, get_texture_rect(game_over_screen), screen_rect, new Vector2(0, 0), 0, Color.WHITE);
 
-    // Raylib.DrawTexture(Raylib.LoadTexture("Assets/Backgrounds/Game over.png"));
+    Raylib.EndDrawing();
 
+    }
+    else if (blocks.Count == 0) // vinner
+    {
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.DARKGRAY);
 
+    Raylib.DrawTexturePro(success_screen, get_texture_rect(success_screen), screen_rect, new Vector2(0, 0), 0, Color.WHITE);
+
+    Raylib.EndDrawing();
     }
 }
 Console.ReadLine();
+
+Rectangle get_texture_rect(Texture2D texture) {return new Rectangle(0, 0, texture.width, texture.height);}
 
 
 void draw_game() {
